579488d00d2e660cd3ec647b9329065f898923da
Pitos <pitos007@gmail.com>
2017-12-01 17:08:10 +0000

    prod

--- src/LambdaStreams/Patterns/Points.java
+++ src/LambdaStreams/Patterns/Points.java
@@ -1,29 +1,29 @@
 package LambdaStreams.Patterns;
 
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Points {
+    private static List<Point> pointList = new ArrayList<>();
     public interface Consumer<T> extends java.util.function.Consumer<T>{
         void accept(T t);
     }
 
-    private class Client{
-        List<Point> pointList = new ArrayList<>();
+    public static void getMaxDist(){
+        List<Integer> intList = Arrays.asList(23,58,6,78,28,97,11,74,63,37,85,24,12);
 
-        public void addPoints(){
-            pointList.add(new Point(1,2));
-            pointList.add(new Point(2,1));
-            pointList.add(new Point(1,1));
-            pointList.add(new Point(2,2));
-
-            pointList.forEach(new Consumer<Point>() {
-                @Override
-                public void accept(Point point) {
-                    point.translate(1,1);
-                }
-            });
-        }
+        OptionalDouble maxDist = intList.parallelStream()
+                .map(i -> new Point(i%3,i/3))
+                .mapToDouble(p -> p.distance(0,0))
+                .max();
+        System.out.println(maxDist);
     }
+
+    public static void main(String[] args) {
+
+        getMaxDist();
+    }
+
 }

33b1aee44a09aefcffb2ec1c2875da8b748b1325
Patryk Jakubik <UPatryk@acushnetgolf.com>
2018-07-19 17:26:02 +0100

    mergin cd

--- src/main/java/rxjavaS/impl/Merging.java
+++ src/main/java/rxjavaS/impl/Merging.java
@@ -2,16 +2,19 @@ package rxjavaS.impl;
 
 import io.reactivex.Observable;
 
+import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Merging {
     public static void main(String[] args) {
-        simpleMarge();
-        flatMapOp();
-        simpleConcat();
-        concatWithTake(2);
+        //simpleMarge();
+        //flatMapOp();
+        //simpleConcat();
+        //concatWithTake(2);
+        //simpleZipping();
+        simpleCombineLatest();
     }
 
 
@@ -78,6 +81,61 @@ public class Merging {
         //keep application alive for 5 seconds
         sleep(5000);
     }
+
+
+
+
+
+    // Zipping allows to take an emission from each Observable source
+    // and combine it into a single emission.
+
+    // if one or more sources are producing emissions faster than another, zip() will queue up
+    // those rapid emissions as they wait on the slower source to provide emissions.
+    // This could cause undesirable performance issues as each source queues in memory.
+
+    private static void simpleZipping(){
+        Observable<String> source1 = Observable.just("One", "Two", "Three", "Four");
+        Observable<Long> source2 = Observable.interval(1, TimeUnit.SECONDS);
+        Observable.zip(source1, source2, (str, sec) -> str)
+                .subscribe(str -> System.out.println(str + " received at " + LocalTime.now()));
+        sleep(4000);
+    }
+
+
+    // combineLatest is a first come first serve solution
+    // where caching and pairing the latest emission is peformed
+
+    private static void simpleCombineLatest(){
+        Observable<Long> source1 = Observable.interval(300, TimeUnit.MILLISECONDS);
+        Observable<Long> source2 = Observable.interval(1, TimeUnit.SECONDS);
+        Observable.combineLatest(source1, source2, (l1,l2) -> "SOURCE 1: " + l1 + " SOURCE 2: " + l2)
+                .subscribe(System.out::println);
+
+        sleep(3000);
+    }
+
+    //SOURCE 1: 0 - no emissions from source 2 yet so this will not print
+    //SOURCE 1: 1 - no emissions from source 2 yet so this will not print
+
+    //SOURCE 1: 2 SOURCE 2: 0
+    //SOURCE 1: 3 SOURCE 2: 0
+    //SOURCE 1: 4 SOURCE 2: 0
+    //SOURCE 1: 5 SOURCE 2: 0
+
+    //SOURCE 1: 5 SOURCE 2: 1 - source 1 starts from the latest emission 5
+    //SOURCE 1: 6 SOURCE 2: 1
+    //SOURCE 1: 7 SOURCE 2: 1
+    //SOURCE 1: 8 SOURCE 2: 1
+    //SOURCE 1: 9 SOURCE 2: 1
+
+    //SOURCE 1: 9 SOURCE 2: 2 - source 1 starts from the latest emission 9
+
+
+
+
+
+
+
 
 
     private static void sleep(long millis) {
